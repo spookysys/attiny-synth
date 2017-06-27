533ee465ceb1d37d8c25284504c6e5b6775c271f
Eivind Liland <spookysys@gmail.com>
2017-06-27 18:01:08 +0200

    Eliminated duplicate drums in amen break to reduce size

--- synth/Player.cpp
+++ synth/Player.cpp
@@ -33,141 +33,122 @@ void Player::render(Buffer &db)
         case 0x0000:
             drumpf.trigger(AMEN_01);
             break;
-        case 0x022d:
-            drumpf.trigger(AMEN_02);
-            break;
-        case 0x0429:
-            drumpf.trigger(AMEN_03);
-            break;
-        case 0x064f:
-            drumpf.trigger(AMEN_04);
-            break;
-        case 0x0751:
-            drumpf.trigger(AMEN_05);
-            break;
-        case 0x084e:
-            drumpf.trigger(AMEN_06);
-            break;
-        case 0x0947:
-            drumpf.trigger(AMEN_07);
-            break;
-        case 0x0a33:
-            drumpf.trigger(AMEN_08);
-            break;
-        case 0x0b61:
-            drumpf.trigger(AMEN_09);
-            break;
-        case 0x0c23:
-            drumpf.trigger(AMEN_10);
-            break;
-        case 0x0e48:
-            drumpf.trigger(AMEN_11);
-            break;
-        case 0x0f51:
-            drumpf.trigger(AMEN_12);
-            break;
         case 0x1041:
-            drumpf.trigger(AMEN_13);
-            break;
-        case 0x123f:
-            drumpf.trigger(AMEN_14);
-            break;
-        case 0x144e:
-            drumpf.trigger(AMEN_15);
-            break;
-        case 0x166e:
-            drumpf.trigger(AMEN_16);
-            break;
-        case 0x1770:
-            drumpf.trigger(AMEN_17);
-            break;
-        case 0x186b:
-            drumpf.trigger(AMEN_18);
-            break;
-        case 0x1951:
-            drumpf.trigger(AMEN_19);
-            break;
-        case 0x1a50:
-            drumpf.trigger(AMEN_20);
-            break;
-        case 0x1b81:
-            drumpf.trigger(AMEN_21);
-            break;
-        case 0x1c5b:
-            drumpf.trigger(AMEN_22);
-            break;
-        case 0x1e5f:
-            drumpf.trigger(AMEN_23);
-            break;
-        case 0x1f72:
-            drumpf.trigger(AMEN_24);
-            break;
         case 0x2052:
-            drumpf.trigger(AMEN_25);
-            break;
-        case 0x225a:
-            drumpf.trigger(AMEN_26);
-            break;
-        case 0x2464:
-            drumpf.trigger(AMEN_27);
-            break;
-        case 0x2660:
-            drumpf.trigger(AMEN_28);
-            break;
-        case 0x276a:
-            drumpf.trigger(AMEN_29);
-            break;
-        case 0x285e:
-            drumpf.trigger(AMEN_30);
-            break;
-        case 0x295b:
-            drumpf.trigger(AMEN_31);
-            break;
-        case 0x2a3a:
-            drumpf.trigger(AMEN_32);
-            break;
-        case 0x2c51:
-            drumpf.trigger(AMEN_33);
-            break;
-        case 0x2e34:
-            drumpf.trigger(AMEN_34);
+            drumpf.trigger(AMEN_13);
             break;
         case 0x304b:
             drumpf.trigger(AMEN_35);
             break;
+
         case 0x315d:
-            drumpf.trigger(AMEN_36);
+            drumpf.trigger(/*AMEN_36*/AMEN_19);
+            break;
+
+        case 0x022d:
+            drumpf.trigger(AMEN_02);
+            break;
+        case 0x123f:
+        case 0x225a:
+            drumpf.trigger(/*AMEN_14*/AMEN_13);
             break;
         case 0x3238:
-            drumpf.trigger(AMEN_37);
+            drumpf.trigger(/*AMEN_37*/AMEN_20);
             break;
+
         case 0x334c:
-            drumpf.trigger(AMEN_38);
+            drumpf.trigger(/*AMEN_38*/AMEN_21);
+            break;
+
+        case 0x0429:
+        case 0x144e:
+        case 0x2464:
+            drumpf.trigger(AMEN_03);
             break;
         case 0x3426:
             drumpf.trigger(AMEN_39);
             break;
+
+        case 0x064f:
+        case 0x166e:
+        case 0x2660:
+            drumpf.trigger(AMEN_04);
+            break;
         case 0x363e:
             drumpf.trigger(AMEN_40);
             break;
+
+        case 0x0751:
+        case 0x1770:
+        case 0x276a:
+            drumpf.trigger(AMEN_05);
+            break;
         case 0x3749:
-            drumpf.trigger(AMEN_41);
+            drumpf.trigger(/*AMEN_41*/AMEN_23);
+            break;
+
+        case 0x084e:
+        case 0x186b:
+        case 0x285e:
+            drumpf.trigger(AMEN_06);
             break;
         case 0x3835:
             drumpf.trigger(AMEN_42);
             break;
+
+        case 0x0947:
+        case 0x1951:
+        case 0x295b:
+            drumpf.trigger(AMEN_07);
+            break;
         case 0x3929:
             drumpf.trigger(AMEN_43);
+            break;
+
+        case 0x0a33:
+        case 0x1a50:
+            drumpf.trigger(AMEN_08);
+            break;
+        case 0x2a3a:
+            drumpf.trigger(AMEN_13);
             break;
         case 0x3a0d:
             drumpf.trigger(AMEN_44);
             break;
+
+        case 0x0b61:
+        case 0x1b81:
+            drumpf.trigger(AMEN_09);
+            break;
+
+        case 0x0c23:
+        case 0x1c5b:
+            drumpf.trigger(AMEN_10);
+            break;
+        case 0x2c51:
+            drumpf.trigger(AMEN_16);
+            break;
         case 0x3c3f:
             drumpf.trigger(AMEN_45);
+            break;
+
+        case 0x0e48:
+        case 0x1e5f:
+            drumpf.trigger(AMEN_11);
+            break;
+        case 0x2e34:
+            drumpf.trigger(/*AMEN_34*/AMEN_15);
             break;
         case 0x3e0b:
             drumpf.trigger(AMEN_46);
             break;
+            
+        case 0x0f51:
+        case 0x1f72:
+            drumpf.trigger(AMEN_12);
+            break;
+
+        default:;
         }
 
         db.clear();
