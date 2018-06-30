243ab21a91ae5a8d23618e94d0e01191f261239c
Eivind Liland <spookysys@gmail.com>
2018-07-01 01:40:34 +0200

    Fixed stack overflow on linux

--- synth/drop.cpp
+++ synth/drop.cpp
@@ -82,7 +82,7 @@ int main(int argc, char* argv[])
     cout << "Rendering" << endl;
     
 
-    Buffer buffs[length_in_buffers];
+    static Buffer buffs[length_in_buffers];
 
     static Player player;
 
