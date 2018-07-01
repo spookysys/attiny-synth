dc510dabefc172d016270f9f5c8e28412ebf065f
Eivind Liland <spookysys@gmail.com>
2018-07-01 10:33:45 +0200

    Added misc.hpp, which has been missing

---
+++ synth/misc.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "Buffer.hpp"
+#include "myrand.hpp"
+
+// high quality lerp from one value to another over a buffer
+static void lerp(int8_t start, int8_t end, Buffer &v)
+{
+    int16_t iter = start << globals::SAMPLES_PER_BUFFER_LOG2;
+    int16_t step = int16_t(end) - int16_t(start);
+    uint16_t dither = myrand::rand16();
+
+    /* approx 10 cycles plus loop */
+    for (uint8_t i = 0; i < globals::SAMPLES_PER_BUFFER; i++)
+    {
+		int16_t tmp = iter >> (globals::SAMPLES_PER_BUFFER_LOG2 - 1);
+		tmp += dither & 1;
+		tmp >>= 1;
+        v[i] += tmp;
+        dither >>= 1;
+        iter += step;
+    }
+}
